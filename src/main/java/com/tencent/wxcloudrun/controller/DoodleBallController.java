706ffc3b8f3c703fa86d178adeb59256dec44d2e
yuanbochang <yuanbochang86@gmail.com>
2024-12-25 23:21:08 +0800

    2024年12月25日17:56:32

--- src/main/java/com/tencent/wxcloudrun/controller/DoodleBallController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/DoodleBallController.java
@@ -3,6 +3,7 @@ package com.tencent.wxcloudrun.controller;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.model.DoodleBallPlayer;
 import com.tencent.wxcloudrun.service.DoodleBallPlayerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author 常源博
  * @since 2024-12-25 16:43
  */
+@Slf4j
 @RestController
 @RequestMapping("doodle-ball")
 public class DoodleBallController {
@@ -43,7 +45,8 @@ public class DoodleBallController {
     @PatchMapping("player")
     public ApiResponse upsertPlayer(@RequestHeader("x-wx-openid")String openId,
             @RequestBody DoodleBallPlayer player) {
-         doodleBallPlayerService.updatePlayer(player);
+        log.info("upsertPlayer: openId={}, player={}", openId, player);
+        doodleBallPlayerService.updatePlayer(player);
         return ApiResponse.ok();
     }
 
